86f099802f9c7c64ef329837de1fa1b082ef9a30
Tim <skidrow430@gmail.com>
2023-05-22 23:48:34 +0300

    git commit -m "Добалена база данных'

---
+++ Проект/База данных/127.0.0.1.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 23 2023 г., 00:40
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.3.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `base2`
+--
+CREATE DATABASE `base2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `base2`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(22) DEFAULT NULL,
+  `login` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Проект/База данных/README.md
@@ -0,0 +1,9 @@
+# Документация к базе данных
+## Содержит таблицу пользователи
+Структура базы данных
+* id (int)
+* username(varchar255)
+* email(varchar255)
+* password(varchar255)
+* tinyint(tinyint1)
+
